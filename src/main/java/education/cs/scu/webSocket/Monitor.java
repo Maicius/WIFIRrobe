5469ef33261db7ba21c4595ebc33c3c43791b56c
mai ciu s <maicius@outlook.com>
2017-06-19 14:32:47 +0800

    跳出率和深访率前后端结合

--- src/main/java/education/cs/scu/webSocket/Monitor.java
+++ src/main/java/education/cs/scu/webSocket/Monitor.java
@@ -2,7 +2,7 @@ package education.cs.scu.webSocket;
 
 import education.cs.scu.DBHelper.DBHelper;
 import education.cs.scu.DBHelper.DataDBManager;
-import education.cs.scu.entity.UserFlow;
+import education.cs.scu.entity.UserVisitBean;
 import education.cs.scu.webSocket.handler.WebSocketEndPointTest;
 
 import java.sql.Connection;
@@ -22,15 +22,19 @@ public class Monitor implements Runnable {
 
     private static ResultSet rs = null;
     private static DataDBManager clockDataDBManager = null;
-    private UserFlow userFlow = new UserFlow();
+    private UserVisitBean userFlow = new UserVisitBean();
 
     public void run() {
         try {
             System.out.println("clockDBManager" + clockDataDBManager.toString());
             rs = clockDataDBManager.executeQuery();
             if (rs.next()) {
-                userFlow.setTimestamp(rs.getTimestamp("time"));
-                userFlow.setTotal_flow(rs.getInt("total_flow"));
+                userFlow.setTime(rs.getString("time"));
+                userFlow.setTotalFlow(rs.getInt("total_flow"));
+                userFlow.setCheckInFlow(rs.getLong("check_in_flow"));
+                userFlow.setCheckInRate(rs.getLong("check_in_rate"));
+                userFlow.setDeepVisitRate(rs.getLong("deep_visit_rate"));
+                userFlow.setShallowVisitRate(rs.getLong("shallow_visit_rate"));
                 WebSocketEndPointTest webSocketTest = new WebSocketEndPointTest();
                 System.out.println("推送消息:" + userFlow);
                 webSocketTest.sendMsg(userFlow);
